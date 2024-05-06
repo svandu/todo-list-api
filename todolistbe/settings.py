c3e21450483f6e9a628cc8d35876b829e484e518
Surbhi Vandana <svandu08@gmail.com>
2024-05-06 15:18:13 +0530

    jwt authentication added

--- todolistbe/settings.py
+++ todolistbe/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
 WSGI_APPLICATION = 'todolistbe.wsgi.application'
 
 # Database
